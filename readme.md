af7c6a134ad61ae8320a41f20d816543a454799c
pepperz <butlerbryan00@gmail.com>
2023-07-06 21:50:08 +1000

    fixed up run_test.sh

--- readme.md
+++ readme.md
@@ -1,8 +1,8 @@
 # Bryan Butler: Banking/ATM app
 
 
-###Presentation video:
-###Repository:
+### Presentation video:
+### Repository:
 
 
 <br>
--- src/run_test.sh
+++ src/run_test.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-python3 -m venv environment
-source environment/bin/activate
+python3 -m venv test_environment
+
+if [ -f "test_environment/Scripts/activate" ]; then
+  source test_environment/Scripts/activate
+else
+  source test_environment/bin/activate
+fi
+
 pip install pytest
 python3 -m pytest ./test.py
 deactivate
-rm -r environment
+rm -r test_environment
